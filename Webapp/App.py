f6ec9f81603ccece67d8817c0d690a99d56b25b4
bartigit <146131119+bartigit@users.noreply.github.com>
2024-06-25 02:34:35 +0200

    small fix

--- Webapp/App.py
+++ Webapp/App.py
@@ -173,7 +173,7 @@ def get_midday_weather_values():
     while True:
         if datetime.datetime.now().time().hour == 14 and datetime.datetime.now().time().minute > 30:
             min_temp_today, max_temp_today, wind_dir_today, wind_speed_today, humidity_today, pressure_today = check_today_weather_values()
-            print("THREAD 1: Weather value are assigned.")
+            print("THREAD 1: Weather values are assigned.")
             time.sleep(7200)
         time.sleep(30)
 
