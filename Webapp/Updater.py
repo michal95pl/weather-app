a6678543fac6d1942d063abb4950268ad1f35a5c
Michal Lichtarski <michal.licht1806@gmail.com>
2024-06-26 21:57:30 +0200

    Updater fix

--- Webapp/Updater.py
+++ Webapp/Updater.py
@@ -3,7 +3,7 @@ import threading
 import time
 from Database.Database import Database
 from Webapp import WeatherAPI
-from App import gmt10
+import pytz
 
 min_temp_today = None
 max_temp_today = None
@@ -14,12 +14,17 @@ pressure_today = None
 
 city = "Canberra"
 
+gmt10 = pytz.timezone(f'Australia/{city}')
+
+automatic_add_flag = True
+get_midday_weather_values_flag = True
 
 # thread functions
 def automatic_add(db: Database):
     global min_temp_today, max_temp_today, wind_dir_today, wind_speed_today, humidity_today, pressure_today
+    global automatic_add_flag
 
-    if datetime.datetime.now().astimezone(gmt10).time().hour == 23 and datetime.datetime.now().astimezone(gmt10).time().minute > 55:
+    if datetime.datetime.now().astimezone(gmt10).time().hour == 23 and datetime.datetime.now().astimezone(gmt10).time().minute > 55 and automatic_add_flag:
         decision = db.decide_if_raining_by_votes()
 
         today = datetime.datetime.today()
@@ -31,14 +36,28 @@ def automatic_add(db: Database):
                                             wind_speed_today, humidity_today, pressure_today, decision_value)
             print("THREAD 1: Weather record was added.")
 
+            # wait for the next day
+            automatic_add_flag = False
+
+    else:
+        automatic_add_flag = True
+
 
 def get_midday_weather_values():
     global min_temp_today, max_temp_today, wind_dir_today, wind_speed_today, humidity_today, pressure_today
+    global get_midday_weather_values_flag
 
-    if ((datetime.datetime.now().astimezone(gmt10).time().hour == 14 and datetime.datetime.now().astimezone(gmt10).time().minute > 30)
-            or min_temp_today is None or max_temp_today is None or wind_dir_today is None or wind_speed_today is None):
+    if (((datetime.datetime.now().astimezone(gmt10).time().hour == 14 and datetime.datetime.now().astimezone(gmt10).time().minute > 30)
+            or min_temp_today is None or max_temp_today is None or wind_dir_today is None or wind_speed_today is None)
+            and get_midday_weather_values_flag):
         min_temp_today, max_temp_today, wind_dir_today, wind_speed_today, humidity_today, pressure_today = WeatherAPI.check_today_weather_values()
         print("THREAD 1: Weather values are assigned.")
+
+        # wait for the next day
+        get_midday_weather_values_flag = False
+
+    else:
+        get_midday_weather_values_flag = True
 
 
 def background_thread():
