f0ff314eaa40301d295d06fb3c5573945d92ca4f
Michal Lichtarski <michal.licht1806@gmail.com>
2024-06-27 22:36:27 +0200

    Revert "fix plot"
    This reverts commit b49dcfe079c990391ce720f6a521f8f662f3e6d3.

--- ModelBuilder/Model.py
+++ ModelBuilder/Model.py
@@ -4,7 +4,6 @@ from sklearn.metrics import f1_score
 from sklearn.ensemble import RandomForestClassifier
 import seaborn as sns
 import joblib
-from random import randint
 
 
 class Model:
@@ -14,31 +13,20 @@ class Model:
     def fit(self, x, y):
         self.__model.fit(x, y)
 
-    @staticmethod
-    def show_plot(cm, title):
+    def show_statistics(self, x, y):
+        y_pred = self.__model.predict(x)
+
+        cm = confusion_matrix(1-y, 1-y_pred, labels=[0, 1])
         plt.figure(figsize=(10, 7))
         sns.heatmap(cm, annot=True, fmt="d", xticklabels=["Rainy", "No Rainy"], yticklabels=["Rainy", "No Rainy"])
         plt.xlabel("Truth")
         plt.ylabel("Predicted")
-        plt.title(title)
         plt.show()
-
-    def show_statistics(self, x, y, city_name):
-        y_pred = self.__model.predict(x)
-
-        cm = confusion_matrix(1 - y, 1 - y_pred, labels=[0, 1])
-
-        if randint(0, 5) == 0:
-            Model.show_plot(cm, city_name)
 
         print("Recall:", cm[0][0] / (cm[0][0] + cm[1][0]))
         print("Precision:", cm[0][0] / (cm[0][0] + cm[0][1]))
         print("F1-score:", f1_score(y, y_pred))
         print("Accuracy:", self.__model.score(x, y))
-
-    def get_F1_score(self, x, y):
-        y_pred = self.__model.predict(x)
-        return f1_score(y, y_pred)
 
     def save_model(self, path):
         joblib.dump(self.__model, path)
--- ModelBuilder/main.py
+++ ModelBuilder/main.py
@@ -5,13 +5,9 @@ from Model import Model
 
 from time import sleep
 
-from statistics import median
-
 if __name__ == "__main__":
     dataset = Dataset()
     dataset.show_correlation()
-
-    F1_scores = []
 
     for location in dataset.get_locations():
 
@@ -29,16 +25,9 @@ if __name__ == "__main__":
 
         model = Model()
         model.fit(X_train, y_train)
-        model.show_statistics(X_test, y_test, location)
-
-        F1_scores.append(model.get_F1_score(X_test, y_test))
+        #model.show_statistics(X_test, y_test)
 
         model.save_model(f"../models/{location}.pkl")
 
         print("\n\n\n")
-
-        sleep(0.5)
-
-    print("F1-score median:", median(F1_scores))
-
 
